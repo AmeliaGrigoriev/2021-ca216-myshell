7dc047f6ce1fa7c6cb1af5ebfc52408208474e7a
grigora2 <amelia.grigoriev2@mail.dcu.ie>
2021-03-25 23:44:35 +0000

    changed functions.h to commands.h

--- src/commands.h
+++ src/commands.h
@@ -32,17 +32,40 @@ void cd(char *argv[])
     }
 }
 
-void dir()
+void dir() // used https://www.tutorialspoint.com/c-program-to-list-all-files-and-sub-directories-in-a-directory
 {
-    char cmd[100];
-    strcpy(cmd, "ls -al");
-    system(cmd);
+    struct dirent *files;
+    DIR *dir = opendir(".");
+    if (dir == NULL){
+        printf("no such file or directory" );
+    }
+    while ((files = readdir(dir)) != NULL)
+    printf("%s\n", files->d_name);
+    closedir(dir);
 }
 
 void pause_shell()
 {
     printf("To continue, press enter.\n");
     while(getchar() != '\n');
+}
+
+void help()
+{
+    printf("Here is a list of commands in myshell:\n");
+    printf("cd\nclr\nenviron\necho\nhelp\npause\nquit\nmore\n");
+}
+
+void more()
+{
+    /*****************************************/
+}
+
+void pwd(char *argv[])
+{
+    char cwd[100];
+    getcwd(cwd, sizeof(cwd));
+    printf("%s\n", cwd);
 }
 
 int ext_commands(char **args)
